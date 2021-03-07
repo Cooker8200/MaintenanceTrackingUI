3765fd8bfdfe0d841df40615f5b0bb8ddb71aa04
Matthew Cook <matthewcook@matthews-mbp.attlocal.net>
2021-03-06 20:59:22 -0600

    added maintenance test data

--- src/components/view/maintenance/Maintenance.tsx
+++ src/components/view/maintenance/Maintenance.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
 import MainAppBar from '../appbar/MainAppBar';
 import { useState } from 'react';
-import { getAllMaintenanceRecords } from '../../actions/MaintenanceActions';
+// import { getAllMaintenanceRecords } from '../../actions/MaintenanceActions';
+import { maintenanceData } from './maintenanceTestData';
 
 const Maintenance: React.FC = () => {
   const [initializing, setInitializing] = useState<boolean>(true);
   const [initialized, setInitialized] = useState<boolean>(false)
+  const [maintenanceRecords, setMaintenanceRecords] = useState<any>([]);
 
   if (initializing === true && initialized === false) {
-    getAllMaintenanceRecords();
+    // getAllMaintenanceRecords();
+    setMaintenanceRecords(maintenanceData)
     setInitializing(false);
-    setInitialized(true);
+    setInitialized(true)
   }
 
   return (
---
+++ src/components/view/maintenance/maintenanceTestData.ts
@@ -0,0 +1,34 @@
+export const maintenanceData = [
+  {
+    repairType: 'Filters change',
+    repairDate: '1/1/2021',
+    cost: '500',
+    paid: true,
+    vendor: 'Rhymes HVAC',
+    equipment: 'HVAC Unit 1',
+  },
+  {
+    repairType: 'Grill Certification',
+    repairDate: '2/1/2021',
+    cost: '250',
+    paid: true,
+    vendor: 'Midwest Equipment',
+    equipment: 'Grill 1'
+  },
+  {
+    repairType: 'Steam cleaning',
+    repairDate: '12/26/2020',
+    cost: '500',
+    paid: true,
+    vendor: 'Midwest Equipment',
+    equipment: 'Shake machine',
+  },
+  {
+    repairType: 'Motor replacement',
+    repairDate: '11/1/2020',
+    cost: '750',
+    paid: true,
+    vendor: 'Midwest Equipment',
+    equipment: 'Shake machine',
+  }
+];
